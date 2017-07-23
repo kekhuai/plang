a402ca4e9700be6707c781207f6593efee4e19ae
Natthaphon Phanit <natthaphon@geniustree.co.th>
2017-07-24 00:03:17 +0700

    implement convert number to thai word function including unit tests

---
+++ plang/plang.py
@@ -0,0 +1,65 @@
+import math
+
+__SMALL = [
+    'ศูนย์', 'หนึ่ง', 'สอง', 'สาม', 'สี่',
+    'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า'
+]
+__DENOM = {2: 'สิบ', 3: 'ร้อย', 4: 'พัน', 5: 'หมื่น', 6: 'แสน', 7: 'ล้าน'}
+
+
+def convert_number_to_thai_word(number):
+    if __number_size(number) > 7:
+        left = (number // 1000000)
+        number = number - (left * 1000000)
+        if number == 0:
+            return convert_number_to_thai_word(left) + 'ล้าน'
+        return convert_number_to_thai_word(left)\
+            + 'ล้าน'\
+            + convert_number_to_thai_word(number)
+    else:
+        thai_word = ''
+        if __is_negative(number):
+            thai_word += 'ลบ'
+            number = abs(number)
+        if number < 10:
+            return thai_word + __SMALL[number]
+        else:
+            while __number_size(number) > 0:
+                left_most_digit = __left_most_digit(number)
+                number_size = __number_size(number)
+                if number_size == 2:
+                    if left_most_digit != 1:
+                        if left_most_digit == 2:
+                            thai_word += 'ยี่'
+                        else:
+                            thai_word += __SMALL[left_most_digit]
+                    thai_word += __DENOM[number_size]
+                elif number_size == 1:
+                    thai_word += 'เอ็ด' if number == 1 else __SMALL[number]
+                else:
+                    thai_word += __SMALL[__left_most_digit(number)]\
+                        + __DENOM[__number_size(number)]
+                number = __remove_left_most_digit(number)
+            return thai_word
+
+
+def __left_most_digit(n):
+    if n < 10:
+        return n
+    else:
+        return __left_most_digit(n // 10)
+
+
+def __number_size(n):
+    n = abs(n)
+    if n == 0:
+        return 0
+    return math.floor(math.log10(n)) + 1
+
+
+def __is_negative(n):
+    return n < 0
+
+
+def __remove_left_most_digit(n):
+    return n - (__left_most_digit(n) * (10 ** (__number_size(n) - 1)))
---
+++ plang/test_plang.py
@@ -0,0 +1,83 @@
+import unittest
+
+import plang
+
+
+class TestPlangMethod(unittest.TestCase):
+
+    def test_invalid_parameter(self):
+        with self.assertRaises(TypeError):
+            plang.convert_number_to_thai_word('any string')
+
+    def test_small_number(self):
+        self.assertEqual(plang.convert_number_to_thai_word(0), 'ศูนย์')
+        self.assertEqual(plang.convert_number_to_thai_word(1), 'หนึ่ง')
+        self.assertEqual(plang.convert_number_to_thai_word(2), 'สอง')
+        self.assertEqual(plang.convert_number_to_thai_word(3), 'สาม')
+        self.assertEqual(plang.convert_number_to_thai_word(4), 'สี่')
+        self.assertEqual(plang.convert_number_to_thai_word(5), 'ห้า')
+        self.assertEqual(plang.convert_number_to_thai_word(6), 'หก')
+        self.assertEqual(plang.convert_number_to_thai_word(7), 'เจ็ด')
+        self.assertEqual(plang.convert_number_to_thai_word(8), 'แปด')
+        self.assertEqual(plang.convert_number_to_thai_word(9), 'เก้า')
+
+    def test_ten(self):
+        self.assertEqual(plang.convert_number_to_thai_word(10), 'สิบ')
+        self.assertEqual(plang.convert_number_to_thai_word(11), 'สิบเอ็ด')
+        self.assertEqual(plang.convert_number_to_thai_word(12), 'สิบสอง')
+        self.assertEqual(plang.convert_number_to_thai_word(20), 'ยี่สิบ')
+        self.assertEqual(plang.convert_number_to_thai_word(22), 'ยี่สิบสอง')
+        self.assertEqual(plang.convert_number_to_thai_word(30), 'สามสิบ')
+
+    def test_hundred(self):
+        self.assertEqual(plang.convert_number_to_thai_word(100), 'หนึ่งร้อย')
+        self.assertEqual(
+            plang.convert_number_to_thai_word(111),
+            'หนึ่งร้อยสิบเอ็ด')
+        self.assertEqual(
+            plang.convert_number_to_thai_word(121),
+            'หนึ่งร้อยยี่สิบเอ็ด')
+
+    def test_million(self):
+        self.assertEqual(
+            plang.convert_number_to_thai_word(1000000),
+            'หนึ่งล้าน')
+        self.assertEqual(
+            plang.convert_number_to_thai_word(1000001),
+            'หนึ่งล้านเอ็ด')
+        self.assertEqual(
+            plang.convert_number_to_thai_word(1000002),
+            'หนึ่งล้านสอง')
+        self.assertEqual(
+            plang.convert_number_to_thai_word(1200000),
+            'หนึ่งล้านสองแสน')
+
+    def test_negative_number(self):
+        self.assertEqual(plang.convert_number_to_thai_word(-1), 'ลบหนึ่ง')
+
+    # @unittest.expectedFailure
+    def test_more_than_million(self):
+        self.assertEqual(
+            plang.convert_number_to_thai_word(10000000),
+            'สิบล้าน'
+        )
+        self.assertEqual(
+            plang.convert_number_to_thai_word(11000000),
+            'สิบเอ็ดล้าน'
+        )
+        self.assertEqual(
+            plang.convert_number_to_thai_word(11000000),
+            'สิบเอ็ดล้าน'
+        )
+        self.assertEqual(
+            plang.convert_number_to_thai_word(1000000000),
+            'หนึ่งพันล้าน'
+        )
+        self.assertEqual(
+            plang.convert_number_to_thai_word(-1000000000),
+            'ลบหนึ่งพันล้าน'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
