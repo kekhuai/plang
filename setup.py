8ea301c3255565681917121551d8568f49de2999
Natthaphon Phanit <natthaphon@geniustree.co.th>
2017-07-24 22:40:08 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup
 
 setup(
     name='plang',
-    version='1.0.0',
+    version='1.0.0-alpha',
     description='This is a python package that convert a number to Thai word',
     author='kekhuay',
     author_email='nulladivides@protonmail.com',
-    license='LICENSE',
+    license='MIT',
     packages=['plang'],
+    keywords='convert number Thai word',
     url='https://github.com/kekhuay/plang')
